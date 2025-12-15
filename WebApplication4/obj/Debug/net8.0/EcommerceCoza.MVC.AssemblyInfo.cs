3d3797a4fa4ee60e89308f51aa3fd2613c6df80a
abbasqaziyev2006 <abbasgg@code.edu.az>
2025-12-16 00:27:22 +0400

    10 product first

--- WebApplication4/obj/Debug/net8.0/EcommerceCoza.MVC.AssemblyInfo.cs
+++ WebApplication4/obj/Debug/net8.0/EcommerceCoza.MVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcommerceCoza.MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a49f9de2a2c211876faf7faffecfe5740b9e39fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e9a24d551da2a587d50a7b4d6a005a535fea316")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcommerceCoza.MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcommerceCoza.MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
