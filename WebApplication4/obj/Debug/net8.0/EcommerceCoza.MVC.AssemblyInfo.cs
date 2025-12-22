bdc773255061f8f1de3e245d8ffaffa0def0d197
abbasqaziyev2006 <abbasgg@code.edu.az>
2025-12-22 17:59:14 +0400

    ccc

--- WebApplication4/obj/Debug/net8.0/EcommerceCoza.MVC.AssemblyInfo.cs
+++ WebApplication4/obj/Debug/net8.0/EcommerceCoza.MVC.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcommerceCoza.MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ae2f9092871b1f9acff54f21e955db077cfaacf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24ffaaebf12c08c4bbf202ef99f87cfb1acba690")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcommerceCoza.MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcommerceCoza.MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
